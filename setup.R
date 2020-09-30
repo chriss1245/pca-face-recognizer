6975c12fe30be8e769ec97e4e6b9c8638c74eccc
madebypixel02 <madebypixel02@gmail.com>
2020-09-30 21:53:03 +0200

    Changed how working directory is set on platforms

--- setup.R
+++ setup.R
@@ -1,10 +1,12 @@
 ## Fixes pca-face-recognizer folder as working directory 
     #All the other files are going to share the same working directory
-setwd(dirname(sys.frame(1)$ofile))
+#setwd(system("pwd", intern = T))
+setwd(dirname(rstudioapi::getSourceEditorContext()$path))
+
 
 ## Installs if needed the packages specified in requirements.txt
     #If there are libraries not added yet, please add them to the txt file
 req <- read.delim('requirements.txt', header = F, sep = "\b")[,1]
-for (i in req) 
-    if (!require(i, character.only = T)) 
+for (i in req)
+    if (!require(i, character.only = T))
         install.packages(i)

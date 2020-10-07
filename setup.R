dd1621c7caa04ff0fad703e5a53a336fa18b605d
Christopher Manzano <36691998+mvca1507@users.noreply.github.com>
2020-10-07 10:41:13 +0200

    added the images

--- setup.R
+++ setup.R
@@ -1,8 +1,8 @@
+rm(list = ls())
 ## Fixes pca-face-recognizer folder as working directory 
     #All the other files are going to share the same working directory
 #setwd(system("pwd", intern = T))
 setwd(dirname(rstudioapi::getSourceEditorContext()$path))
-
 
 ## Installs if needed the packages specified in requirements.txt
     #If there are libraries not added yet, please add them to the txt file
@@ -10,3 +10,5 @@ req <- read.delim('requirements.txt', header = F, sep = "\b")[,1]
 for (i in req)
     if (!require(i, character.only = T))
         install.packages(i)
+rm(i, req)
+
